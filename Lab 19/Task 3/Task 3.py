e4201c79164e5cc5e421adcdf52326601eae1528
Joer4765 <joer.tuhn@gmail.com>
2023-11-22 19:04:41 +0200

    new interface based on grid

--- Lab 19/Task 3/Task 3.py
+++ Lab 19/Task 3/Task 3.py
@@ -6,75 +6,82 @@ class ImageManipulationApp:
         self.root = root
         self.root.title("Image Manipulation App")
 
+        # Canvas
         self.canvas = Canvas(root, bg="white", width=500, height=500)
-        self.canvas.pack()
+        self.canvas.grid(row=0, column=0, columnspan=4)
 
-        self.image_path = None
-        self.original_image = None
-        self.current_image = None
-
+        # Buttons
         self.load_button = Button(root, text="Load Image", command=self.load_image)
-        self.load_button.pack()
+        self.load_button.grid(row=1, column=0, pady=5)
 
+        self.apply_filter_button = Button(root, text="Apply Filter", command=self.apply_filter)
+        self.apply_filter_button.grid(row=1, column=1, pady=5)
+
+        self.rotate_button = Button(root, text="Rotate", command=self.rotate_image)
+        self.rotate_button.grid(row=1, column=2, pady=5)
+
+        self.crop_button = Button(root, text="Crop", command=self.crop_image)
+        self.crop_button.grid(row=1, column=3, pady=5)
+
+        self.resize_button = Button(root, text="Resize", command=self.resize_image)
+        self.resize_button.grid(row=2, column=0, pady=5)
+
+        self.mirror_button = Button(root, text="Mirror", command=self.mirror_image)
+        self.mirror_button.grid(row=2, column=1, pady=5)
+
+        self.overlay_button = Button(root, text="Overlay", command=self.overlay_images)
+        self.overlay_button.grid(row=2, column=2, pady=5)
+
+        self.save_button = Button(root, text="Save Image", command=self.save_image)
+        self.save_button.grid(row=2, column=3, pady=5)
+
+        # Labels and Entries
         self.rotation_label = Label(root, text="Enter Rotation Angle:")
-        self.rotation_label.pack()
+        self.rotation_label.grid(row=3, column=0)
 
         self.rotation_entry = Entry(root)
-        self.rotation_entry.pack()
+        self.rotation_entry.grid(row=3, column=1)
 
         self.crop_label = Label(root, text="Enter Crop Coordinates (x1, y1, x2, y2):")
-        self.crop_label.pack()
+        self.crop_label.grid(row=4, column=0)
 
         self.crop_entry = Entry(root)
-        self.crop_entry.pack()
+        self.crop_entry.grid(row=4, column=1)
 
         self.resize_label = Label(root, text="Enter Resize Dimensions (width, height):")
-        self.resize_label.pack()
+        self.resize_label.grid(row=5, column=0)
 
         self.resize_entry = Entry(root)
-        self.resize_entry.pack()
+        self.resize_entry.grid(row=5, column=1)
 
+        self.opacity_label = Label(root, text="Enter Opacity (0-255):")
+        self.opacity_label.grid(row=6, column=0)
+
+        self.opacity_entry = Entry(root)
+        self.opacity_entry.grid(row=6, column=1)
+
+        # Listboxes
         self.orientation_label = Label(root, text="Choose Orientation:")
-        self.orientation_label.pack()
+        self.orientation_label.grid(row=3, column=2)
 
         self.orientation_listbox = Listbox(root, selectmode="single", height=7)
         orientations = ["FLIP_LEFT_RIGHT", "FLIP_TOP_BOTTOM", "ROTATE_90", "ROTATE_180", "ROTATE_270", "TRANSPOSE", "TRANSVERSE"]
         for orientation in orientations:
             self.orientation_listbox.insert(END, orientation)
-        self.orientation_listbox.pack()
+        self.orientation_listbox.grid(row=3, column=3)
 
         self.filter_label = Label(root, text="Choose Filter:")
-        self.filter_label.pack()
+        self.filter_label.grid(row=4, column=2)
 
         self.filter_listbox = Listbox(root, selectmode="single", height=3)
         filters = ["BLUR", "CONTOUR", "DETAIL"]
         for filter_name in filters:
             self.filter_listbox.insert(END, filter_name)
-        self.filter_listbox.pack()
+        self.filter_listbox.grid(row=4, column=3)
 
-        self.apply_filter_button = Button(root, text="Apply Filter", command=self.apply_filter)
-        self.apply_filter_button.pack()
-
+        # Info Label
         self.info_label = Label(root, text="File Information:")
-        self.info_label.pack()
-
-        self.rotate_button = Button(root, text="Rotate", command=self.rotate_image)
-        self.rotate_button.pack()
-
-        self.crop_button = Button(root, text="Crop", command=self.crop_image)
-        self.crop_button.pack()
-
-        self.resize_button = Button(root, text="Resize", command=self.resize_image)
-        self.resize_button.pack()
-
-        self.mirror_button = Button(root, text="Mirror", command=self.mirror_image)
-        self.mirror_button.pack()
-
-        self.overlay_button = Button(root, text="Overlay", command=self.overlay_images)
-        self.overlay_button.pack()
-
-        self.save_button = Button(root, text="Save Image", command=self.save_image)
-        self.save_button.pack()
+        self.info_label.grid(row=5, column=2, columnspan=2)
 
     def load_image(self):
         self.image_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
@@ -181,8 +188,20 @@ class ImageManipulationApp:
             overlay_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
             if overlay_path:
                 overlay_image = Image.open(overlay_path)
+
+                try:
+                    opacity = int(self.opacity_entry.get())
+                    if not (0 <= opacity <= 255):
+                        raise ValueError("Opacity must be between 0 and 255.")
+                except ValueError:
+                    print("Please enter a valid opacity value (0-255).")
+                    return
+
                 overlay_image = overlay_image.resize(self.original_image.size)
-                result_image = Image.alpha_composite(self.original_image.convert("RGBA"), overlay_image.convert("RGBA"))
+                overlay_image = overlay_image.convert("RGBA")
+                overlay_image.putalpha(opacity)
+
+                result_image = Image.alpha_composite(self.original_image.convert("RGBA"), overlay_image)
                 self.show_image(result_image)
                 self.display_file_info()
 
@@ -195,8 +214,8 @@ class ImageManipulationApp:
     def save_image(self):
         if self.current_image:
             save_path = filedialog.asksaveasfilename(defaultextension=".jpg",
-                                                     filetypes=[("All files", "*.*"), ("PNG files", "*.png"),
-                                                                ("JPEG files", "*.jpg")])
+                                                     filetypes=[("PNG files", "*.png"), ("JPEG files", "*.jpg"),
+                                                                ("All files", "*.*")])
             if save_path:
                 img_pil = ImageTk.getimage(self.current_image)
                 img_pil.save(save_path)
